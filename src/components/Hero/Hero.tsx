953b1ad7f2add280c3437c2421d474e5ed4105d3
Yuri Boblak <160606809+sselesnes@users.noreply.github.com>
2025-07-28 09:02:21 +0300

    hero arrows

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,19 +1,24 @@
 import css from "./Hero.module.css";
 import car from "../../assets/alfa-romeo-147.webp";
+import arrow_right from "../../assets/arrow-right.svg";
+// ;
 
 export default function Hero() {
   return (
     <section role="banner" aria-labelledby="hero-title">
       <h1 className={css.banner_main}>
-        Терміновий викуп будь-яких<br></br>автомобілів в Харкові та області
+        Терміновий викуп будь-яких
+        <br />
+        автомобілів в Харкові та області
       </h1>
       <p className={css.banner_content}>
         Отримайте до 95% вартості Вашого авто на руки вже сьогодні
       </p>
       <h2 className={css.banner_sub}>
+        <img className={css.arrow_left} src={arrow_right} alt="Arrow" />
         <a href="tel:+380956196756">Зателефонуйте</a> або залиште заявку
+        <img className={css.arrow_right} src={arrow_right} alt="Arrow" />
       </h2>
-      {/* <p className={css.banner_content}>Ми виплатимо вам всю суму готівкою вже сьогодні</p> */}
       <img className={css.car} src={car} alt="car" />
     </section>
   );
