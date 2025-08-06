24c82b35c76d28ed49d9d436149efdce692c7162
Yuri Boblak <sseslesnes@gmail.com>
2025-08-06 17:02:14 +0300

    contact2 arrow

--- src/components/Contact2/Contact2.tsx
+++ src/components/Contact2/Contact2.tsx
@@ -1,20 +1,36 @@
 import css from "./Contact2.module.css";
+import arrow_right from "../../assets/arrow-right.svg";
+
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 type ModalProps = {
   openModal: () => void;
 };
 
 export default function Contact2({ openModal }: ModalProps) {
+  useGSAP(() => {
+    // Animate arrow_left
+    gsap.to(`.${css.arrow_left}`, {
+      x: 8, // Move 8px to the right
+      duration: 0.5,
+      repeat: -1, // Repeat indefinitely
+      yoyo: true, // Reverse back to original position
+      ease: "power1.inOut", // Smooth easing
+    });
+  }, []);
+
   return (
     <section role="region" aria-labelledby="contact2-title">
       <h1 className={css.banner_main}>Час діяти</h1>
       <p className={css.banner_content}>
-        Лише один крок відділяє вас від мети. Залиште свою заявку і наша команда допоможе Вам
+        Лише один крок відділяє Вас від мети. Залиште свою заявку і наша команда допоможе Вам
         отримати кошти швидко та комфортно.
       </p>
       {/* Викликаємо openModal, переданий з App */}
       <button className={css.button} onClick={openModal}>
         Отримати кошти
+        <img className={css.arrow_left} src={arrow_right} alt="Arrow" />
       </button>
     </section>
   );
