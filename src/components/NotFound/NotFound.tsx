708f069ca6c6ab30f6b1d0d218ac2352726ba308
Yuri Boblak <sseslesnes@gmail.com>
2025-08-25 13:18:05 +0300

    404 css

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -1,14 +1,13 @@
 // import React from "react";
 import css from "../App/App.module.css";
-
 import Header from "../Header/Header";
 
 export default function NotFound() {
   return (
     <div className={css.container}>
       <Header />
-      <h1>404 - Сторінку не знайдено</h1>
-      <p>Вибачте, але сторінка, яку ви шукаєте, не існує.</p>
+      <h1 className={css.error_title}>404 - Сторінку не знайдено</h1>
+      <p className={css.error_text}>Вибачте, але сторінка, яку ви шукаєте, не існує.</p>
     </div>
   );
 }
